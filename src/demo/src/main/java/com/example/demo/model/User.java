b48082dce4e115a43999c042de2cc7b38e5d0d71
Marinas2001 <p.marinas.2019@alumnos.urjc.es>
2025-02-19 13:45:13 +0100

    Creacion de user para tablas

--- src/demo/src/main/java/com/example/demo/model/User.java
+++ src/demo/src/main/java/com/example/demo/model/User.java
@@ -1,5 +1,61 @@
 package com.example.demo.model;
+import jakarta.persistence.*;
+import lombok.*;
 
+@Entity
 public class User {
+
+    @Id
+	@GeneratedValue(strategy = GenerationType.AUTO)
+	private long id;
+
+    private String username;
+    private String password;
+    private String email;
     
+
+    protected User() {
+        // Used by JPA
+    }
+
+    public User(String username, String password, String email) {
+        this.username = username;
+        this.password = password;
+        this.email = email;
+    }
+
+    // Getters
+    public String getUsername() {
+        return username;
+    }
+
+    public String getPassword() {
+        return password;
+    }
+
+    public String getEmail() {
+        return email;
+    }
+
+    // Setters
+    public void setUsername(String username) {
+        this.username = username;
+    }
+
+    public void setPassword(String password) {
+        this.password = password;
+    }
+
+    public void setEmail(String email) {
+        this.email = email;
+    }
+
+
+    @Override
+	public String toString() {
+		return String.format("Customer[id=%d, username='%s', pasword='%s', email='%s']",
+				id, username, password, email);
+	}
 }
+
+
