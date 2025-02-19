ed6d78ab01247c084ed6af8d720b4cf9b15754f3
SergiOnLive3 <adriserg2000@gmail.com>
2025-02-19 14:31:12 +0100

    Clase Comentario

---
+++ src/demo/src/main/java/com/example/demo/model/Comment.java
@@ -0,0 +1,22 @@
+public class Comment {
+    private String comment;
+    private User user;
+
+    public Comment(String comment, User user) {
+        this.comment = comment;
+        this.user = user;
+    }
+
+    public String getComment() {
+        return this.comment;
+    }
+    public void setComment(String comment) {
+        this.comment = comment;
+    }
+    public User getUser() {
+        return this.user;
+    }
+    public void setUser(User user) {
+        this.user = user;
+    }
+}
