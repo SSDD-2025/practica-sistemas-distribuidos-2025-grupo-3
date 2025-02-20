5d382fe4dfe0f076073fd134326c877af9f10390
Alonso_DS <alonsogalerna14@gmail.com>
2025-02-20 16:38:13 +0100

    Community page to add post, comments

--- src/demo/src/main/java/com/example/demo/Controller/WebController.java
+++ src/demo/src/main/java/com/example/demo/Controller/WebController.java
@@ -4,6 +4,7 @@ import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Controller;
 import org.springframework.ui.Model;
 import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PathVariable;
 
 import com.example.demo.Service.CommunityService;
 
@@ -20,6 +21,14 @@ public class WebController {
         model.addAttribute("comunities", cs.findAll());
         return "home";
     }
+
+    @GetMapping("/communities/{id}")
+    public String showCommunity(Model model, @PathVariable Long id) {
+        model.addAttribute("community", cs.findById(id));
+        model.addAttribute("id", id);
+        return "community";
+    }
+    
 
     @GetMapping("/user_main_page")
     public String login() {
