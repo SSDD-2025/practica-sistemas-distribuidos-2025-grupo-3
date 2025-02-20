d5224f74676af5f966b687b1b0d7bb44b56e4cd7
Nicolasghubv67 <59668074+Nicolasghubv67@users.noreply.github.com>
2025-02-20 09:50:49 +0100

    Class comment fixed
    Removed attribute from class comment and minos style changes in styles.css and header

--- src/demo/src/main/java/com/example/demo/model/Comment.java
+++ src/demo/src/main/java/com/example/demo/model/Comment.java
@@ -12,36 +12,27 @@ public class Comment {
     @GeneratedValue(strategy = GenerationType.AUTO)
     private Long id;
     private String comment;
-    private User user;
+
 
     protected Comment() {
         // Used by JPA
     }
 
-    public Comment(String comment, User user) {
+    public Comment(String comment) {
         this.comment = comment;
-        this.user = user;
     }
 
     public String getComment() {
         return this.comment;
     }
 
-    public User getUser() {
-        return this.user;
-    }
-
     public void setComment(String comment) {
         this.comment = comment;
-    }
-    
-    public void setUser(User user) {
-        this.user = user;
     }
 
     @Override
 	public String toString() {
 		return String.format("Comment[id=%d, comment='%s', user='%s']",
-				id, comment, user);
+				id, comment);
 	}
 }
