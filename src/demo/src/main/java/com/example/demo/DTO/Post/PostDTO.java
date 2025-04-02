0654f6b11e016a07f1fb7dae1d16d4c9a639ae5b
Alonso_DS <alonsogalerna14@gmail.com>
2025-04-02 13:13:08 +0200

    DTOs to record

--- src/demo/src/main/java/com/example/demo/DTO/Post/PostDTO.java
+++ src/demo/src/main/java/com/example/demo/DTO/Post/PostDTO.java
@@ -6,6 +6,6 @@ import com.example.demo.model.Comment;
 import com.example.demo.model.Community;
 import com.example.demo.model.User;
 
-public record PostDTO(Long id, String title, String postContent, User userName, Community community, List<Comment> comments) {
+public record PostDTO(Long id, String title, String postContent, User userName, Community community) {
 
 }
